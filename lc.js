ec2d00b11a6be8a27185ab9b028beb1969204d4d
zeroIsScared <69678222+zeroIsScared@users.noreply.github.com>
2023-07-03 17:00:18 +0300

    Update lc.js
    modified HW

--- lc.js
+++ lc.js
@@ -54,32 +54,36 @@ let bYears = new Collection([
 
 bYears.add(2019);
 
-if (bYears.lenght === 7) {
+if (bYears.length == 7) {
+    console.log(bYears)
     console.log('test 1 passed!');
 
 } else {
+    console.log(bYears)
     console.error('test 1 FAILED!')
 }
 
 //test case 2 
-let fYears = bYears.filter(1999, 2000);
+let fYears = bYears.filter(y => y >= 1999 && y <= 2009 ? true : false);
 
-if (fYears.length == 2) {
+if (fYears.length == 3) {
+    console.log(fYears)
     console.log('test 2 passed!');
 
 } else {
+    console.log(fYears)
     console.error('test 2 FAILED!')
 }
 ////////////////////////////////
 //main code
 
-let paymens = new Collection([
+let payments = new Collection([
     { amount: 100, currency: 'EUR', date: '2023-01-01' },
     { amount: 200, currency: 'USD', date: '2023-01-02' },
     { amount: 300, currency: 'EUR', date: '2023-01-03' },
 ])
 
-paymens.add({ amount: 400, currency: 'EUR', date: '2023-01-04' })
+payments.add({ amount: 400, currency: 'EUR', date: '2023-01-04' })
 
 
 // Ioc
@@ -90,6 +94,4 @@ paymens.add({ amount: 400, currency: 'EUR', date: '2023-01-04' })
 // how can we pass parametres (min, max)
 //bind 
 //call apply
-let mPayments = payments.filter((v) => {
-    return v.amount >= 200 && v.amount <= 300;
-})
+let mPayments = payments.filter(v => v.amount >= 200 && v.amount <= 300 ? true : false)
